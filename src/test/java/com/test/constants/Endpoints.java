a072eb60efb00777d0a122276582fde2f4138211
Anusha8025 <119311936+Anusha8025@users.noreply.github.com>
2023-12-01 07:05:31 -0600

    Update Endpoints.java

--- src/test/java/com/test/constants/Endpoints.java
+++ src/test/java/com/test/constants/Endpoints.java
@@ -1,11 +1,11 @@
 package com.test.constants;
 
 public class Endpoints {
-	public static final String LOGIN="/login";
-	public static final String GET_DATA="/getdata";
-	public static final String ADD_DATA="/addData";
-	public static final String UPADATE_DATA="/updateData";
-	public static final String DELETE_DATA="/deleteData";
-	public static final String GETSINGLEREPO = null;
+	public static final String GETSINGLEREPO="repos/Anusha8025/";
+	public static final String GETALLREPO=" Anusha8025/repos";
+	public static final String CREATEREPO="Anusha8025/repos";
+	public static final String UPADATEREPO="repos/Anusha8025";
+	public static final String DELETEREPO="repos/Anusha8025";
+	public static final String TOKEN="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";
 	
 }
